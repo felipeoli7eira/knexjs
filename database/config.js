2c8a37f6ec1c7adfd0d72bb29d165b65c8bb8a6e
felipeoli7eira <me.felipeoliveira@gmail.com>
2020-12-04 20:34:24 -0300

    base

---
+++ database/config.js
@@ -0,0 +1,14 @@
+const knex = require('knex')
+(
+    {
+        clint: 'mysql2',
+        connection: {
+            host: 'localhost',
+            user: 'felipe',
+            password: 'root',
+            database: 'knex'
+        }
+    }
+)
+
+module.exports = knex
