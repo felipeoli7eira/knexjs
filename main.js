59c7e8bd5941be5c0df824f2f26bfe64a3c6ad59
felipeoli7eira <me.felipeoliveira@gmail.com>
2020-12-05 17:59:45 -0300

    select, select-where and delete

--- main.js
+++ main.js
@@ -1,24 +1,71 @@
 const Database = require('./database/config')
 
-let data = {
-    name: 'CS:GO',
-    price: 29.90
-}
-
-/*
-    let data = [
-        {....},
-        {....},
-        {....},
-        {....},
-        {....}
-    ]
+/** insert
+*
+* Database.insert({column: value...}).into('tableName').then().catch()
 */
 
-const query = Database.insert(data).into('games').then(response => {
 
-    console.log(response)
 
-}).catch(error => console.log(error))
+/** select
+*
+* Database.select().table('games').then(data => {...}).catch(error => {...})
+*
+* Database.select(['fieldName', 'fieldName']).table('games').then(data => {...}).catch(error => {...})
+*
+* Database.select().table('games').then(data => {
+** Database.select().table('user').then(...).catch(....)
+* }).catch(error)
+*/
 
-// console.log(query.toQuery())
+/** select with where
+
+    Database
+    .where({id: 1})
+    .table('games')
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+    Database
+    .select(['id', 'price'])
+    .where({id: 1})
+    .table('games')
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+    Database
+    .select(['id', 'price'])
+    .where({id: 1})
+    .where({name: 'example'})
+    .table('games')
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+    Database
+    .select(['id', 'price'])
+    .where({id: 1})
+    .where({name: 'CS:GO'})
+    .table('games')
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+    Database
+    .select()
+    .whereRaw('name = ... OR price > ...')
+    .table('games')
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+    Database.raw('SELECT * FROM games') <- qualquer código SQL. a raw() function recebe uma sql query qualquer. livre.
+*/
+
+/** Delete
+
+Database
+.table('games')
+.where({id: 5})
+.delete()
+.then(data => console.log(data))
+.catch(error => console.log(error)
+
+*/
